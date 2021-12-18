e88b68609e4cf7b0326fd0206bd2d75c529a3bfd
hamza <hamza.benkhoud@etudiant-isi.utm.tn>
2021-12-18 15:31:43 +0100

    solution

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
-    }
-})
+   data() {
+      return {
+         product: 'Socks',
+         image: './assets/images/socks_blue.jpg',
+         inStock: true,
+         details: ['50% cotton', '30% wool', '20% polyester'],
+         sizes: [39, 40, 41, 42, 43, 44, 45, 46],
+      };
+   },
+});
